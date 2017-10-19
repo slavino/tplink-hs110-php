54f9681cd0b3fd60750b5c9da8907519cfff3c17
Slavomir Hustaty <slavomir.hustaty@gmail.com>
2017-10-19 21:11:27 +0200

    adding switch led off

--- src/TPLinkHS110Command.php
+++ src/TPLinkHS110Command.php
@@ -7,6 +7,8 @@ class TPLinkHS110Command {
     const SWITCH_ON = '{"system":{"set_relay_state":{"state":1}}}';
 
     const SWITCH_OFF = '{"system":{"set_relay_state":{"state":0}}}';
+
+    const SWITCH_LED_OFF = '{"system":{"set_led_off":{"off":1}}}';
     
     //more commands available here: https://github.com/softScheck/tplink-smartplug/blob/master/tplink-smarthome-commands.txt
     
